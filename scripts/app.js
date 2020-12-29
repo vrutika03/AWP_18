217758558d06cc840d510ead8340cc10787886e2
vrutika03 <59534606+vrutika03@users.noreply.github.com>
2020-12-29 14:08:56 +0530

    sixth commit

---
+++ scripts/app.js
@@ -0,0 +1,30 @@
+(function() {
+    const buttons = document.querySelectorAll('.counterBtn')
+    let count = 0
+
+
+    //Add event listeners and functionailty to each button  
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            if (button.classList.contains('prevBtn')) {
+                count--
+            } else if (button.classList.contains('nextBtn')) {
+                count++
+             } else if (button.classList.contains('resetBtn')) {
+                    count = 0
+            }
+
+            //Select the counter text
+            const counter = document.querySelector('#counter')
+            counter.textContent = count
+
+            if (count < 0) {
+                counter.style.color = 'white'
+            } else if (count > 0) {
+                counter.style.color = 'black'
+            } else {
+                counter.style.color = '#333333'
+            }
+        })
+    })
+})()
